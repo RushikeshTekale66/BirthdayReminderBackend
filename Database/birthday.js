5a27ba7ab14290f160166d4d509035b260cef5e8
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-22 07:35:01 +0530

    Updated the Date datatype & connection URL

--- Database/birthday.js
+++ Database/birthday.js
@@ -3,7 +3,7 @@ const mongoose = require ('mongoose');
 let BirthdayReminderSchema = mongoose.Schema({
     name:String,
     mobile:Number,
-    dob:String
+    dob:Date
 })
 
 module.exports= mongoose.model("Birthdays", BirthdayReminderSchema);
--- Database/connect.js
+++ Database/connect.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Url ='mongodb://127.0.0.1:27017/BirthdayReminder'
 
 
-try{
-    mongoose.connect(Url);
-    console.log("Connected to database");
-}
-catch(error){
-    console.log("Unable to connect database");
-}
+// try{
+//     mongoose.connect(Url);
+//     console.log("Connected to database");
+// }
+// catch(error){
+//     console.log("Unable to connect database");
+// }
+
+mongoose.connect(Url).then(console.log("Connected to database")).catch((error)=>console.log("Error ", error));
